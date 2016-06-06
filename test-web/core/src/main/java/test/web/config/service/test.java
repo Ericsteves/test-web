4217a7efb0ff2eeef1aed63e84a3a5a677e6a7f4
hhjhbhj <hhjhbhj@163.com>
2016-06-06 18:55:17 +0800

    initial,index could be shown

---
+++ test-web/core/src/main/java/test/web/config/service/test.java
@@ -0,0 +1,7 @@
+package test.web.config.service;
+
+/**
+ * Created by lenovo on 2016/6/6.
+ */
+public interface test {
+}
---
+++ test-web/web/src/main/java/test/web/controller/IndexController.java
@@ -0,0 +1,60 @@
+package test.web.controller;
+
+/**
+ * Created by lenovo on 2016/6/6.
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by david on 9/20/15.
+ */
+@Controller
+public class IndexController {
+  private static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+  @RequestMapping(value = "/login", method = RequestMethod.GET)
+  public String login(HttpSession session) {
+    //if(session.getAttribute("user") != null)
+    //    return "redirect:/home";
+
+    System.out.println("========== login =========== ");
+
+    return "account/login";
+  }
+
+  @RequestMapping(value = "/{module}", method = RequestMethod.GET)
+  public String module(HttpSession session, ModelMap model, @PathVariable(value = "module") String module) {
+    model.put("module", module);
+
+    logger.debug("[moduleAndView1] redirect to view : " + module);
+    logger.info("[moduleAndView1] redirect to view : " + module);
+
+    return module;
+  }
+
+  @RequestMapping(value = "/{module}/{view}.html", method = RequestMethod.GET)
+  public ModelAndView moduleAndView1(HttpSession session, ModelMap model
+      , @PathVariable(value = "module") String module
+      , @PathVariable(value = "view") String view) {
+    logger.debug("[moduleAndView1] redirect to view : " + module + "&" + view);
+
+    ModelAndView viewModel = new ModelAndView(module + "/" + view);
+    viewModel.setViewName(module + "/" + view);
+
+    model.put("module", module);
+    model.put("view", view);
+
+    return viewModel;
+  }
+}
+
